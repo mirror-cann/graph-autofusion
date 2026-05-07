fe46f614d636042e4200041f418e38d9942ee96e
zhuhaoran5 <zhuhaoran5@huawei.com>
2026-05-07 14:13:21 +0800

    [chore]: Support Q2 building optimization - Support cmake targets 'runtime_headers' 'metadef_headers'
    Co-authored-by: zhuhaoran5<zhuhaoran5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !337 merge support_Q2_building_optimization into master
    
    【chore】: 支持Q2工程优化
    
    Created-by: zhuhaoran5
    Commit-by: zhuhaoran5
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    支持Q2工程优化：
    1. 支持  cmake targets 'runtime_headers' 'metadef_headers'
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [x] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1. 按照 README 部署构建环境
    2. 执行所有构建场景的脚本命令，能执行成功并且产物包内容与先前一致
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/graph-autofusion!337

--- cmake/modules/Findmetadef.cmake
+++ cmake/modules/Findmetadef.cmake
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------------------
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 # CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -8,22 +8,7 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------------------------------------
 
-set(CMAKE_PREFIX_PATH $ENV{ASCEND_HOME_PATH}/)
-
-set(CMAKE_MODULE_PATH
-  ${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules
-  ${CMAKE_MODULE_PATH}
+add_library(metadef_headers INTERFACE)
+set_target_properties(metadef_headers PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ""
 )
-message(STATUS "CMAKE_MODULE_PATH:${CMAKE_MODULE_PATH}")
-
-if (NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type(default Release)"
-    FORCE)
-endif ()
-
-find_package(runtime MODULE REQUIRED)
-find_package(unified_dlog MODULE REQUIRED)
-find_package(securec MODULE REQUIRED)
-
-include(cmake/third_party/json.cmake)
-
