84907e5d5079e6d16a25acdb7b76ea2b534a09af
wei_shi <huangqianqian15@huawei.com>
2026-05-29 17:42:51 +0800

    【Fix】:910B/910C reduce max/any删除int32类型的ascir
    Co-authored-by: wei_shi<huangqianqian15@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !529 merge master into develop
    
    【Fix】:910B/910C reduce max/any删除int32类型的ascir
    
    Created-by: wei_shi
    Commit-by: wei_shi
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    910B/910C reduce max/any删除不支持的int32类型的ascir
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    910B环境下执行调用ReduceMax/ReduceAny接口，输入参数为int类型时不进行融合
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [ ] 我的代码遵循了项目的代码风格
    - [ ] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [ ] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [ ] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/graph-autofusion!529

--- autofuse/ascir/generator/ascir_builtin_ops_v1.cpp
+++ autofuse/ascir/generator/ascir_builtin_ops_v1.cpp
@@ -349,7 +349,7 @@ REG_ASC_IR(Max)
     .ComputeType(ComputeType::kComputeReduce)
     .Impl(v1_soc_versions, {af::ascir::AscIrImplCreator<af::ascir::MaxAscIrAttImpl>(),
                             af::ascir::AscIrImplCreator<af::ascir::MaxAscIrCodegenImpl>(),
-                            {{"T", TensorType{DT_FLOAT16, DT_FLOAT, DT_INT32}}}});
+                            {{"T", TensorType{DT_FLOAT16, DT_FLOAT}}}});
 
 REG_ASC_IR(Sum)
     .Input("x", "T")
@@ -397,7 +397,7 @@ REG_ASC_IR(Any)
     .ComputeType(ComputeType::kComputeReduce)
     .Impl(v1_soc_versions, {af::ascir::AscIrImplCreator<af::ascir::AnyAscIrAttImpl>(),
                             af::ascir::AscIrImplCreator<af::ascir::AnyAscIrCodegenImpl>(),
-                            {{"T", TensorType{DT_FLOAT, DT_INT32}}}});
+                            {{"T", TensorType{DT_FLOAT}}}});
 
 REG_ASC_IR(All)
     .Input("x", "T")
